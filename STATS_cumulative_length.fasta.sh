bc4a6d6ec3af43397403d2bd3d83e59cabf2f52d
Shannon EK Joslin <sejoslin@ucdavis.edu>
2022-08-31 13:28:25 -0700

    remove tmp fasta file

--- STATS_cumulative_length.fasta.sh
+++ STATS_cumulative_length.fasta.sh
@@ -14,9 +14,11 @@ fileend=$(echo $1 | rev | cut -f 1 -d. | rev)
 if [ $fileend = 'gz' ]
 then
     zcat $1 | perl -pe '/^>/ ? print "\n" : chomp' | tail -n +2 > ${1}.tmp && cat ${1}.tmp | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
+    rm ${1}.tmp
 elif [ $fileend = 'fasta' ] || [ $fileend = 'fa' ]
 then
     cat $1 | perl -pe '/^>/ ? print "\n" : chomp' | tail -n +2 > ${1}.tmp && cat ${1}.tmp | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
+    rm ${1}.tmp
 else
     echo "File does not have one of the following endings: .gz .fasta .fa"
 fi

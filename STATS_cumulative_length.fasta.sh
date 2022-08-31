d63ba60dcea9ac41c893bea050eb4611ef693490
Shannon EK Joslin <sejoslin@ucdavis.edu>
2022-08-31 13:27:34 -0700

    allow for 2+ line fasta files

--- STATS_cumulative_length.fasta.sh
+++ STATS_cumulative_length.fasta.sh
@@ -4,6 +4,8 @@
 # Date      : 31 August 2022
 
 # This file will take in a fasta file and produce a cumulative length file for plotting/visualization.
+##      It shouldn't matter if you have a two line fasta or multiple... this will convert & delete excess file...right now it is memory intensive, as it saves a new files then deletes. You should clean it up to convert the stdout of the first perl fxn
+
 # RUN CMD = bash STATS_cumulative_length.fasta.sh <fasta_file> <output_prefix>
 # INPUT = gzipped or not fasta
 # OUTPUT = | read length | cumulative length |
@@ -11,10 +13,10 @@
 fileend=$(echo $1 | rev | cut -f 1 -d. | rev)
 if [ $fileend = 'gz' ]
 then
-    zcat $1 | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
+    zcat $1 | perl -pe '/^>/ ? print "\n" : chomp' | tail -n +2 > ${1}.tmp && cat ${1}.tmp | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
 elif [ $fileend = 'fasta' ] || [ $fileend = 'fa' ]
 then
-    cat $1 | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
+    cat $1 | perl -pe '/^>/ ? print "\n" : chomp' | tail -n +2 > ${1}.tmp && cat ${1}.tmp | perl -ne '$id=$_; $seq=<>; chomp $seq; print length($seq)."\n"' | sort -rn | perl -ne '$len=$_; chomp $len; $all+=$len; print "$len\t$all\n"' > ${2}.cl
 else
     echo "File does not have one of the following endings: .gz .fasta .fa"
 fi

9287ce02aa50a587ef07f76c5d06209368534eb5
Shannon EK Joslin <sejoslin@ucdavis.edu>
2022-11-08 07:24:13 -0800

    update to go from perl script to sorted

--- dissertation/SexMarker-sort_counts.sh
+++ dissertation/SexMarker-sort_counts.sh
@@ -2,7 +2,13 @@
 
 set -x
 
+# infile should be file with all loci totals (haven't removed 0's yet)
 infile=$1
-outfile=$2
 
-sort -nrk 10 ${infile} >> ${outfile}
+# read in header line
+header=$(sed -n 1p ${infile}) 
+grep -v $'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0'$'\t''0' ${infile} > ${infile}.no0.counts
+echo ${header} > ${infile}.no0.counts.sort
+sort -nrk 9 ${infile}.no0.counts >> ${infile}.no0.counts.sort
+rm ${infile}.no0.counts
+gzip ${infile}
